3cceb0d44a9a37196ed28ae2ac980a7a4ddc0faf
Mathis Vialle <mathis.vialle@epitech.eu>
2024-07-14 01:27:12 +0200

    :sparkles: Introduce new moving algo mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 const { x: cursorX, y: cursorY } = useMouse()
 
-const ballX = ref(0)
-const ballY = ref(0)
+const BALL_HOME = { x: 54, y: 24 }
+
+const ballX = ref(BALL_HOME.x)
+const ballY = ref(BALL_HOME.y)
 const speed = 5
 
-const updatePosition = () => {
-  const dx = cursorX.value - ballX.value
-  const dy = cursorY.value - ballY.value
+const moveTo = (posX: number, posY: number) => {
+  const dx = posX - ballX.value
+  const dy = posY - ballY.value
   const distance = Math.sqrt(dx * dx + dy * dy)
 
   if (distance > 1) {
@@ -17,7 +19,39 @@ const updatePosition = () => {
     ballX.value += vx
     ballY.value += vy
   }
+}
 
+const updatePosition = () => {
+  // Check if cursor is inside a card element
+  const safePlacesDOM = document.querySelector('.safe-place')
+
+  if (!safePlacesDOM) {
+    // Ball return to the house by moving at the same speed
+
+    moveTo(cursorX.value, cursorY.value)
+    requestAnimationFrame(updatePosition)
+    return
+  }
+
+  const safePlaces = safePlacesDOM.getBoundingClientRect()
+
+  if (
+    cursorX.value > safePlaces.left
+    && cursorX.value < safePlaces.right
+    && cursorY.value > safePlaces.top
+    && cursorY.value < safePlaces.bottom
+  ) {
+    if (ballX.value === BALL_HOME.x && ballY.value === BALL_HOME.y) {
+      requestAnimationFrame(updatePosition)
+      return
+    }
+
+    moveTo(BALL_HOME.x, BALL_HOME.y)
+    requestAnimationFrame(updatePosition)
+    return
+  }
+
+  moveTo(cursorX.value, cursorY.value)
   requestAnimationFrame(updatePosition)
 }
 
@@ -28,26 +62,30 @@ onMounted(() => {
 
 <template>
   <div
-    class="ball"
     :style="{ transform: `translate(${ballX}px, ${ballY}px)` }"
+    class="ball absolute"
   />
-  <div class="container">
-    <p>Let start coding</p>
-    <p>
-      x: {{ cursorX }} y: {{ cursorY }}
-    </p>
+  <div class="h-full w-full">
+    <UCard class="w-16 h-16 ms-8 mt-8" />
+    <div class="flex justify-center items-center h-full">
+      <UCard
+        class="min-w-[600px] safe-place dark:hover:background-gradient"
+      >
+        <template #header>
+          <p>Let start coding</p>
+        </template>
+
+        <p>
+          x: {{ cursorX }} y: {{ cursorY }}
+        </p>
+
+        <template #footer />
+      </UCard>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-}
-
+<style lang="scss" scoped>
 .ball {
   width: 16px;
   height: 16px;
